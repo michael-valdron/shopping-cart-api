ae550952cc040f04b33d32dab10d77744a85a6f2
Michael Valdron <michael.valdron@gmail.com>
2021-06-27 05:15:07 -0400

    heroku production init query added.

---
+++ queries/create_tables.sql
@@ -0,0 +1,31 @@
+-- Author: Michael Valdron
+
+-- Creates carts relation to store all cart records.
+CREATE TABLE IF NOT EXISTS carts(
+    cart_id SERIAL PRIMARY KEY,
+    cart_subtotal NUMERIC NOT NULL,
+    cart_discount NUMERIC NOT NULL,
+    cart_taxes NUMERIC NOT NULL,
+    cart_total NUMERIC NOT NULL
+);
+
+-- Creates items relation to store all item records.
+-- Each item references a cart.
+CREATE TABLE IF NOT EXISTS items(
+    item_id SERIAL PRIMARY KEY,
+    cart_id INT NOT NULL,
+    item_label VARCHAR(255) NOT NULL,
+    item_price NUMERIC NOT NULL,
+    UNIQUE (cart_id, item_label),
+    CONSTRAINT fk_cart
+        FOREIGN KEY(cart_id)
+        REFERENCES carts(cart_id)
+        ON DELETE CASCADE
+);
+
+-- Creates tax relation to store all tax percentages.
+CREATE TABLE IF NOT EXISTS tax(
+    tax_id SERIAL PRIMARY KEY,
+    tax_label VARCHAR(3) UNIQUE NOT NULL,
+    tax_percent NUMERIC NOT NULL
+);
